88c9fe05e8f44d0c5302796c91a8417afdde8223
Meghan <mmhade@gmail.com>
2015-08-03 14:17:19 -0700

    add table view, links

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model: function() {
-		return this.store.find('operator');
+		return this.store.findAll('operator');
+		// return Ember.RSVP.hash({
+  //           operators: this.store.findAll('operator'),
+  //           feeds: this.store.findAll('feed')
+  //       });
 	}
 });
