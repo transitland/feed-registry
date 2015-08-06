895ecce24551b566e330b4077af4839693b3a49d
Meghan <mmhade@gmail.com>
2015-08-06 16:10:45 -0700

    better table formatting, begin to add in logic for icons

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,16 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	allCountries: [],
-	countryCount: allCountries.length,
-	uniqueCountries: Ember.computed.uniq('countryCount'),
-	test: function(){
-		// console.log(this.operator);
-	}.property(),
-	countCountries: function(){
-		for (var agency in operator){
-			console.log(operator.name);
-		}
-	}.property()
+	// allCountries: [],
+	// countryCount: allCountries.length,
+	// uniqueCountries: Ember.computed.uniq('countryCount'),
+	// test: function(){
+	// 	// console.log(this.operator);
+	// }.property(),
+	// countCountries: function(){
+	// 	for (var agency in operator){
+	// 		console.log(operator.name);
+	// 	}
+	// }.property()
 });
 
