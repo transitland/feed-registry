ca0a9c517a1dd100470899536dea940986f3913a
Meghan <mmhade@gmail.com>
2015-08-18 14:54:27 -0700

    dynamic url for feed detail page includes operator name

--- app/operator/route.js
+++ app/operator/route.js
@@ -2,10 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   // model: function(params) {
-    // return Ember.RSVP.hash({
-    //   operator: this.store.query('operator', { name: params.operator.name }).then((operator) => {
-    //     return operator.get('firstObject');
-    //   })
-    // });
+  //   return Ember.RSVP.hash({
+  //     operator: this.store.query('operator', { name: params.operator.name }).then((operator) => {
+  //       return operator.get('firstObject');
+  //     })
+  //   });
   // }
 });
