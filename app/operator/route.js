0b85a36c90c8e2fa5001708f209db8a71942989f
Meghan <mmhade@gmail.com>
2015-08-18 17:28:45 -0700

    now able to access model data on feed detail page

--- app/operator/route.js
+++ app/operator/route.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
-		return this.store.findRecord('operator');
+  model: function(params) {
+		return this.store.find('operator', params['name']);
 	}
 });
