30e4391f5cfbb358b54bc498cd2768ccae5ee7a0
Meghan <mmhade@gmail.com>
2015-10-01 10:22:44 -0700

    fix json endpoint/url helper

--- app/helpers/json-endpoint.js
+++ app/helpers/json-endpoint.js
@@ -5,16 +5,15 @@ import ENV from 'feed-registry/config/environment';
 export default Ember.Handlebars.registerBoundHelper('json-endpoint',function(param, text) {
   var entity = '';
   var baseURL = ENV.datastoreHost+'/api/v1/';
-  var endpoint = baseURL + entity + param;
 
   if (param.charAt(0) === 'o'){
     entity = 'operators/';
   } else if (param.charAt(0) === 'f'){
     entity = 'feeds/';
   }
-  
+
   if (param.charAt(1) === '-'){
-    return new Ember.Handlebars.SafeString('<a href =' + endpoint +' target = "_blank">' + endpoint + '</a>');
+    return new Ember.Handlebars.SafeString('<a href =' + baseURL + entity + param +' target = "_blank">' + baseURL + entity + param + '</a>');
   } else if (param.charAt(0) === '/') {
     return new Ember.Handlebars.SafeString('<a href =' + ENV.datastoreHost + param + ' target = "_blank">' + text + '</a>');
   } else {
