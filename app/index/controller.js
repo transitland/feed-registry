c3d73d0cbce35c69eb5d2c4354371f76e229f3e6
Meghan <mmhade@gmail.com>
2015-10-14 16:10:41 -0700

    Fix link-to so that it creates one a-tag and does not pass in a model

--- app/index/controller.js
+++ app/index/controller.js
@@ -4,7 +4,6 @@ import ENV from 'feed-registry/config/environment';
 
 export default Ember.Controller.extend({
 	editingMode: Ember.computed(function(){
-		// console.log("env: "+ENV.environment);
 		return ENV.allowEditingMode;
 	})
 });
