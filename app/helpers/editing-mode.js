2579f47ea27e7f63b29d4938e90d018ebdf48edf
Meghan <mmhade@gmail.com>
2015-10-14 15:38:06 -0700

    delete unneeded helper

--- app/helpers/editing-mode.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-import ENV from 'feed-registry/config/environment';
-
-
-export default Ember.Handlebars.registerBoundHelper('editng-mode',function() {
-	return ENV.allowEditingMode;
-});
