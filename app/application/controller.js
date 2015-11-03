ca7ad8cc00d72c4fbed7c012564a86fab28f88c9
Meghan <mmhade@gmail.com>
2015-11-03 14:31:00 -0800

    add google analytics via ember cli

--- app/application/controller.js
+++ app/application/controller.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
+// import ENV from 'feed-registry/config/environment';
+
 
 export default Ember.Controller.extend({
+	// analyticsKey: Ember.computed(function(){
+	// 	return ENV.analyticsKey;
+	// })
 });
