aaded1b98fbfc049bfae8fe220abb57cb8e15505
Meghan <mmhade@gmail.com>
2015-11-03 17:12:21 -0800

    remove commented-out code

--- app/application/controller.js
+++ app/application/controller.js
@@ -1,9 +1,5 @@
 import Ember from 'ember';
-// import ENV from 'feed-registry/config/environment';
 
 
 export default Ember.Controller.extend({
-	// analyticsKey: Ember.computed(function(){
-	// 	return ENV.analyticsKey;
-	// })
 });
