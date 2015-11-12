309322fba35b592e4a88b7b65bac4f5d425eb108
Meghan <mmhade@gmail.com>
2015-11-12 14:06:48 -0800

    add more attributes to test feed

--- app/feeds/new/add-operator/route.js
+++ app/feeds/new/add-operator/route.js
@@ -7,7 +7,15 @@ export default Ember.Route.extend({
     // These are fake operators to use for styling the form
     // TODO: remove and replace with operators actually parsed from GTFS
     feed.get('operators').createRecord({
-      name: 'BART'
+      name: 'Bay Area Rapid Transit',
+      short_name: 'BART',
+      country: 'United States',
+      state: 'California',
+      metro: 'San Francisco Bay Area',
+      website: 'www.bart.com',
+      timezone: 'PCT'
+
+
     });
     return feed;
   }
