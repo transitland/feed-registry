3d556d29bd5f6a9fe4f91485456eedb1d3e1c297
Drew Dara-Abrams <drew@mapzen.com>
2015-11-11 16:38:41 -0800

    Operator improvements
    Operator form now binds to operators associated with feed model. Add operators attribute to feed model, with a hasMany relationship. Update the input field format for the operator form.

--- app/feeds/new/add-operator/route.js
+++ app/feeds/new/add-operator/route.js
@@ -1,22 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  createFeedFromGtfsService: Ember.inject.service('create-feed-from-gtfs'),
   model: function() {
-    return this.store.createRecord('operator', { });
-  },
-  actions: {
-    submit: function() {
-      var operator = this.currentModel;
-      // TODO: actually compute real Onestop ID from short name and geometry
-      operator.set('id', 'o-9q9-FAKE');
-      var changeset = operator.toChangeset();
-      changeset.save();
-    },
-    emailSignup: function(){
-      console.log("sucessful email signup");
-    },
-    addOperatorForm: function() {
-      return("{{operator-form operator=model}}");
-    }
+    return this.get('createFeedFromGtfsService').createOrFindFeedModel();
   }
 });
