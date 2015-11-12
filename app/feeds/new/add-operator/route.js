a7bdaa3dc25dc16a33777e7762ae1e4d88aac5fc
Drew Dara-Abrams <drew@mapzen.com>
2015-11-11 17:34:59 -0800

    add operator(s) form now has some faked operators
    In the future, when we can parse GTFS, we'll remove these.

--- app/feeds/new/add-operator/route.js
+++ app/feeds/new/add-operator/route.js
@@ -3,6 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   createFeedFromGtfsService: Ember.inject.service('create-feed-from-gtfs'),
   model: function() {
-    return this.get('createFeedFromGtfsService').createOrFindFeedModel();
+    var feed = this.get('createFeedFromGtfsService').createOrFindFeedModel();
+    // These are fake operators to use for styling the form
+    // TODO: remove and replace with operators actually parsed from GTFS
+    feed.get('operators').createRecord({
+      name: 'BART'
+    });
+    return feed;
   }
 });
