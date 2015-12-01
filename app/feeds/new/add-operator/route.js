d56617e5ab2217e2ada1ce5bbfd13a2592c08dd8
Ian Rees <ian@ianrees.net>
2015-12-01 14:34:48 -0800

    Remove dummy operator data

--- app/feeds/new/add-operator/route.js
+++ app/feeds/new/add-operator/route.js
@@ -3,20 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   createFeedFromGtfsService: Ember.inject.service('create-feed-from-gtfs'),
   model: function() {
-    var feed = this.get('createFeedFromGtfsService').createOrFindFeedModel();
-    // These are fake operators to use for styling the form
-    // TODO: remove and replace with operators actually parsed from GTFS
-    feed.get('operators').createRecord({
-      name: 'Bay Area Rapid Transit',
-      short_name: 'BART',
-      country: 'United States',
-      state: 'California',
-      metro: 'San Francisco Bay Area',
-      website: 'www.bart.com',
-      timezone: 'PCT'
-
-
-    });
-    return feed;
+    return this.get('createFeedFromGtfsService').createOrFindFeedModel();
   }
 });
