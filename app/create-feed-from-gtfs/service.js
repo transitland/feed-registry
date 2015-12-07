2d02c607414f3cadaac4cf1c19909afdafd8dbac
Ian Rees <ian@ianrees.net>
2015-12-07 14:04:01 -0800

    Max retries: 60

--- app/create-feed-from-gtfs/service.js
+++ app/create-feed-from-gtfs/service.js
@@ -10,9 +10,12 @@ export default Ember.Service.extend({
     }
     return this.get('feedModel');
   },
-  downloadGtfsArchiveAndParseIntoFeedModel: function() {
+  downloadGtfsArchiveAndParseIntoFeedModel: function(retries) {
+    retries = (retries || 60);
+    if (retries-- <= 0) {
+      throw "Timeout"
+    }
     var feedModel = this.createOrFindFeedModel();
-    // var url = 'http://www.caltrain.com/Assets/GTFS/caltrain/GTFS-Caltrain-Devs.zip';
     var url = feedModel.get('url');
     var adapter = this.get('store').adapterFor('feeds');
     var fetch_info_url = adapter.urlPrefix()+'/feeds/fetch_info';
@@ -20,12 +23,10 @@ export default Ember.Service.extend({
     var promise = adapter.ajax(fetch_info_url, 'post', {data:{url:url}});
     promise.then(function(response) {
       if (response.status == 'complete') {
-        // feedModel.update(response.feed);
         feedModel.set('id', response.feed.onestop_id);
-        //feedModel.get('operators').unloadAll();
         return response.operators.map(function(operator){feedModel.addOperator(operator)});
       } else if (response.status == 'processing') {
-        return Ember.run.later(controller, function(){this.downloadGtfsArchiveAndParseIntoFeedModel()}, 1000);
+        return Ember.run.later(controller, function(){this.downloadGtfsArchiveAndParseIntoFeedModel(retries)}, 1000);
       }
     });
     return promise
