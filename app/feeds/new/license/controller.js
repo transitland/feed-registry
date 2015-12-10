c2a740623247ec776b49d1f3e1b4cc1c4176a456
Meghan <mmhade@gmail.com>
2015-12-09 17:05:53 -0800

    add funtions to license controller to set derived-product and redistribute on feed

--- app/feeds/new/license/controller.js
+++ app/feeds/new/license/controller.js
@@ -15,6 +15,12 @@ export default Ember.Controller.extend({
     },
     setAttribution: function(attribution){
       this.model.set("license_use_without_attribution", attribution);
+    },
+    setCreateDerived: function(derived){
+      this.model.set("license_create_derived_product", derived);
+    },
+    setRedistribute: function(redistribute){
+      this.model.set("license_redistribute", redistribute);
     }
   }
 });
