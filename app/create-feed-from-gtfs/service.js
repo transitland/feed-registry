b51c3f1c72caa00628ceca560361175b661b6bd4
Ian Rees <ian@ianrees.net>
2015-12-09 23:01:35 -0800

    Move to fetchinfo route for now...

--- app/create-feed-from-gtfs/service.js
+++ app/create-feed-from-gtfs/service.js
@@ -10,27 +10,6 @@ export default Ember.Service.extend({
     }
     return this.get('feedModel');
   },
-  downloadGtfsArchiveAndParseIntoFeedModel: function(retries) {
-    retries = (retries || 60);
-    if (retries-- <= 0) {
-      throw "Timeout"
-    }
-    var feedModel = this.createOrFindFeedModel();
-    var url = feedModel.get('url');
-    var adapter = this.get('store').adapterFor('feeds');
-    var fetch_info_url = adapter.urlPrefix()+'/feeds/fetch_info';
-    var controller = this;
-    var promise = adapter.ajax(fetch_info_url, 'post', {data:{url:url}});
-    promise.then(function(response) {
-      if (response.status == 'complete') {
-        feedModel.set('id', response.feed.onestop_id);
-        return response.operators.map(function(operator){feedModel.addOperator(operator)});
-      } else if (response.status == 'processing') {
-        return Ember.run.later(controller, function(){this.downloadGtfsArchiveAndParseIntoFeedModel(retries)}, 1000);
-      }
-    });
-    return promise
-  },
 	toChangeset: function() {
     var feedModel = this.createOrFindFeedModel();
     var changes = [];
