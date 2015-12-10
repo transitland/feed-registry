d56db21b83d18f5f0d31a83c74c834e74efd4032
Meghan <mmhade@gmail.com>
2015-12-09 18:08:52 -0800

    updating buttons to control flow

--- app/feeds/new/license/controller.js
+++ app/feeds/new/license/controller.js
@@ -2,17 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   createFeedFromGtfsService: Ember.inject.service('create-feed-from-gtfs'),
-  actions: {
-    submit: function() {
-      console.log("SUBMIT");
-      var changeset = this.get('createFeedFromGtfsService').toChangeset();
-      console.log(changeset);
-      changeset.save().then(function(){
-        controller.transitionToRoute('feeds.new.submit');
-      }).catch(function(){
-        alert('Error with submission');
-      })
-
-    }
   }
 });
