4e24e03cd8301ca6fc46296f006f3a3a56f5b2a1
Drew Dara-Abrams <drew@mapzen.com>
2015-12-22 09:37:24 -0800

    break license step into multiple substeps

--- app/feeds/new/license/controller.js
+++ app/feeds/new/license/controller.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   createFeedFromGtfsService: Ember.inject.service('create-feed-from-gtfs'),
+  license_present_mode: false,
+  interpret_license_mode: false,
   showLicenseAttributionText: Ember.computed('model.license_use_without_attribution', function() {
     if (this.get('model.license_use_without_attribution') === 'no') {
       return true;
