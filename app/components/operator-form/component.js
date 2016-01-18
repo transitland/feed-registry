f6ff154a064476c8d9b858f0ed22ac1f998711a4
Meghan <mmhade@gmail.com>
2016-01-18 14:41:30 -0800

    incorporating @drewda's suggestions to remove isIncluded attribute and refer directly to operator.include_in_changeset, use only one set of buttons, and remove timezone array.

--- app/components/operator-form/component.js
+++ app/components/operator-form/component.js
@@ -1,54 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	// isIncluded: Ember.computed('model.include_in_changeset', function() {
-	// 	if (this.get('operator.include_in_changeset') === true) {
-	// 		return true;
-	// 	} else {
-	// 		return false;
-	// 	}
-	// }),
-	isIncluded: true,
 	actions: {
 		clickYes() {
 			this.set('operator.include_in_changeset', true);
-			this.set('isIncluded', true);
 		},
 		clickNo() {
 			this.set('operator.include_in_changeset', false);
-			this.set('isIncluded', false);
 		}
-	},
-	timezones: [
-		"America/New_York",
-		 "America/Detroit",
-		 "America/Kentucky/Louisville",
-		 "America/Kentucky/Monticello",
-		 "America/Indiana/Indianapolis",
-		 "America/Indiana/Vincennes",
-		 "America/Indiana/Winamac",
-		 "America/Indiana/Marengo",
-		 "America/Indiana/Petersburg",
-		 "America/Indiana/Vevay",
-		 "America/Chicago",
-		 "America/Indiana/Tell_City",
-		 "America/Indiana/Knox",
-		 "America/Menominee",
-		 "America/North_Dakota/Center",
-		 "America/North_Dakota/New_Salem",
-		 "America/North_Dakota/Beulah",
-		 "America/Denver",
-		 "America/Boise",
-		 "America/Phoenix",
-		 "America/Los_Angeles",
-		 "America/Metlakatla",
-		 "America/Anchorage",
-		 "America/Juneau",
-		 "America/Sitka",
-		 "America/Yakutat",
-		 "America/Nome",
-		 "America/Adak",
-		 "Pacific/Honolulu"
-	]
-
+	}
 });
