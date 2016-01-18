5ea23c4a3943c1ccd3a483d7d810d946cd1f4a73
Meghan <mmhade@gmail.com>
2016-01-18 13:02:19 -0800

    UI to toggle to include or disinclude operator in changeset

--- app/components/operator-form/component.js
+++ app/components/operator-form/component.js
@@ -1,6 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	// isIncluded: Ember.computed('model.include_in_changeset', function() {
+	// 	if (this.get('operator.include_in_changeset') === true) {
+	// 		return true;
+	// 	} else {
+	// 		return false;
+	// 	}
+	// }),
+	isIncluded: true,
+	actions: {
+		clickYes() {
+			this.set('operator.include_in_changeset', true);
+			this.set('isIncluded', true);
+		},
+		clickNo() {
+			this.set('operator.include_in_changeset', false);
+			this.set('isIncluded', false);
+		}
+	},
 	timezones: [
 		"America/New_York",
 		 "America/Detroit",
--- app/feeds/new/add-operator/controller.js
+++ app/feeds/new/add-operator/controller.js
@@ -1,8 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	include_operator_mode: false,
-	actions: {
-
-	}
 });
