466959ed91658d4a91759f714a4bbfb4216c2828
Meghan <mmhade@gmail.com>
2016-01-19 15:06:53 -0800

    more attempts to count operators included in changeset

--- app/feeds/new/add-operator/controller.js
+++ app/feeds/new/add-operator/controller.js
@@ -1,10 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	operatorsIncluded: Ember.computed.mapBy('operators', 'include_in_changeset'),
+	numberIncluded: Ember.computed.uniq('operatorsIncluded'),
 
 	singleOperator: Ember.computed('model.operators_in_feed', function(){
 		if (this.minimumOperator !== 0){
 			console.log("minimumOperator !== 0");
+			console.log("operatorsIncluded: " + operatorsIncluded);
 		}
 		return this.get('model.operators_in_feed').length === 1;
 	}),
@@ -13,7 +16,11 @@ export default Ember.Controller.extend({
 	// 	return this.operator.filterBy('this.model.operator.include_in_changeset', true).get('length') >= 1; 
 	// })
 	minimumOperator: function() {
-		console.log('minimumOperator: ' + this.get('model.operators').filterBy('include_in_changeset', true).get('length'));
-		return this.get('model.operators').filterBy('include_in_changeset', true).get('length');
-	}.property('model.operators@each.include_in_changeset')
+		var operators = this.get('model.operators');
+
+		// console.log('minimumOperator: ' + operators.filterBy('include_in_changeset', true).get('length'));
+		return operators.filterBy('include_in_changeset', true).get('length');
+	}.property()
 });
+
+
