e99165ff4af72f13a76c8dd698dd212c05119783
Meghan <mmhade@gmail.com>
2016-01-19 13:33:14 -0800

    starting to add in logic to render submit button only when at least one operator is marked to include in changeset

--- app/components/operator-form/component.js
+++ app/components/operator-form/component.js
@@ -1,14 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	operatorsIncluded: Ember.computed.mapBy('operators', 'name'),
-	uniqueOperators: Ember.computed.uniq('operatorsIncluded'),
-	operatorsInFeed: Ember.get('this.operatorsInFeed'),
-	singleOperator: function(){
-		return this.uniqueOperators.length === 1;
-		// return true;
-	},
-
+	
 	actions: {
 		clickYes() {
 			this.set('operator.include_in_changeset', true);
--- app/feeds/new/add-operator/controller.js
+++ app/feeds/new/add-operator/controller.js
@@ -3,7 +3,17 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
 	singleOperator: Ember.computed('model.operators_in_feed', function(){
+		if (this.minimumOperator !== 0){
+			console.log("minimumOperator !== 0");
+		}
 		return this.get('model.operators_in_feed').length === 1;
-	})
+	}),
 
+	// minimumOperator: Ember.computed('this.model.operator.@each.include_in_changeset', function(){
+	// 	return this.operator.filterBy('this.model.operator.include_in_changeset', true).get('length') >= 1; 
+	// })
+	minimumOperator: function() {
+		console.log('minimumOperator: ' + this.get('model.operators').filterBy('include_in_changeset', true).get('length'));
+		return this.get('model.operators').filterBy('include_in_changeset', true).get('length');
+	}.property('model.operators@each.include_in_changeset')
 });
