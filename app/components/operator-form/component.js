e251df8a96c76e0153e6fb0946669cd5822fe16d
Meghan <mmhade@gmail.com>
2016-01-19 11:57:24 -0800

    toggle to include operator only shows if there are multiple operators in feed

--- app/components/operator-form/component.js
+++ app/components/operator-form/component.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	operatorsIncluded: Ember.computed.mapBy('operators', 'name'),
+	uniqueOperators: Ember.computed.uniq('operatorsIncluded'),
+	operatorsInFeed: Ember.get('this.operatorsInFeed'),
+	singleOperator: function(){
+		return this.uniqueOperators.length === 1;
+		// return true;
+	},
 
 	actions: {
 		clickYes() {
--- app/feeds/new/add-operator/controller.js
+++ app/feeds/new/add-operator/controller.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+	numberOfOperators: Ember.computed('model.operators_in_feed', function(){
+		return this.get('model.operators_in_feed').length === 1;
+	})
+
 });
