aae0c747e0116fea4e24a79a9f143068653a0f2f
Meghan <mmhade@gmail.com>
2016-01-19 11:59:23 -0800

    update variable name

--- app/feeds/new/add-operator/controller.js
+++ app/feeds/new/add-operator/controller.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-	numberOfOperators: Ember.computed('model.operators_in_feed', function(){
+	singleOperator: Ember.computed('model.operators_in_feed', function(){
 		return this.get('model.operators_in_feed').length === 1;
 	})
 
