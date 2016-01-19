bebb1b0f8e2a26b4026baa1a697aef709d6231d8
Ian Rees <ian@ianrees.net>
2016-01-19 15:52:02 -0800

    Copy over errors from bad response

--- app/feeds/new/index/route.js
+++ app/feeds/new/index/route.js
@@ -2,11 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	beforeModel: function(){
-		this.store.unloadAll();
+		// this.store.unloadAll();
 	},
 	createFeedFromGtfsService: Ember.inject.service('create-feed-from-gtfs'),
 		model: function() {
-		this.get('createFeedFromGtfsService').createFeedModel();
-		return this.get('createFeedFromGtfsService').feedModel;
+      console.log('feeds.new.index model');
+      var oldModel = this.get('createFeedFromGtfsService').feedModel;
+      this.store.unloadAll();
+      var newModel = this.get('createFeedFromGtfsService').createFeedModel();
+      if (oldModel != null && oldModel.get('errors.length') > 0) {
+        console.log('setting url:', oldModel.get('url'));
+        newModel.set('url', oldModel.get('url'));
+        oldModel.get('errors').errorsFor('url').forEach(function(error){
+          console.log(error.attribute, error.message);
+          newModel.get('errors').add(error.attribute, error.message);
+        });
+      }
+      return newModel
 	}
 });
