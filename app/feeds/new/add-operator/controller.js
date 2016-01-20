228d87853e70afdbee655b0effa4be9e41c72119
Meghan <mmhade@gmail.com>
2016-01-19 17:02:14 -0800

    set up way to dynamically track whether there is at least one operator included in changeset

--- app/feeds/new/add-operator/controller.js
+++ app/feeds/new/add-operator/controller.js
@@ -1,26 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	operatorsIncluded: Ember.computed.mapBy('operators', 'include_in_changeset'),
-	numberIncluded: Ember.computed.uniq('operatorsIncluded'),
+	operatorsIncluded: Ember.computed.mapBy('model.operators', 'include_in_changeset'),
 
 	singleOperator: Ember.computed('model.operators_in_feed', function(){
-		if (this.minimumOperator !== 0){
-			console.log("minimumOperator !== 0");
-			console.log("operatorsIncluded: " + operatorsIncluded);
-		}
 		return this.get('model.operators_in_feed').length === 1;
 	}),
 
-	// minimumOperator: Ember.computed('this.model.operator.@each.include_in_changeset', function(){
-	// 	return this.operator.filterBy('this.model.operator.include_in_changeset', true).get('length') >= 1; 
-	// })
-	minimumOperator: function() {
+	minimumOperator: Ember.computed('model.operators.@each.include_in_changeset', function(){
 		var operators = this.get('model.operators');
-
-		// console.log('minimumOperator: ' + operators.filterBy('include_in_changeset', true).get('length'));
-		return operators.filterBy('include_in_changeset', true).get('length');
-	}.property()
+		return operators.filterBy('include_in_changeset', true).get('length'); 
+	})
+	
 });
 
 
