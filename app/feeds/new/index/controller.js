58e65c758e0fcd5f8f5d3f8ee95c3555d4060df7
Ian Rees <ian@ianrees.net>
2016-01-19 17:08:15 -0800

    Display loading spinner, remove after transition to add-operator

--- app/feeds/new/index/controller.js
+++ app/feeds/new/index/controller.js
@@ -4,6 +4,7 @@ export default Ember.Controller.extend({
   createFeedFromGtfsService: Ember.inject.service('create-feed-from-gtfs'),
   actions: {
     next: function() {
+      this.toggleProperty('isLoading');
       this.transitionToRoute('feeds.new.add-operator');
     }
   }
--- app/feeds/new/index/route.js
+++ app/feeds/new/index/route.js
@@ -2,16 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	createFeedFromGtfsService: Ember.inject.service('create-feed-from-gtfs'),
-		model: function() {
-      var oldModel = this.get('createFeedFromGtfsService').feedModel;
-      this.store.unloadAll();
-      var newModel = this.get('createFeedFromGtfsService').createFeedModel();
-      if (oldModel != null && oldModel.get('errors.length') > 0) {
-        newModel.set('url', oldModel.get('url'));
-        oldModel.get('errors').errorsFor('url').forEach(function(error){
-          newModel.get('errors').add(error.attribute, error.message);
-        });
-      }
-      return newModel
-	}
+	model: function() {
+    var oldModel = this.get('createFeedFromGtfsService').feedModel;
+    this.store.unloadAll();
+    var newModel = this.get('createFeedFromGtfsService').createFeedModel();
+    // Copy over previous url & errors
+    if (oldModel != null && oldModel.get('errors.length') > 0) {
+      newModel.set('url', oldModel.get('url'));
+      oldModel.get('errors').errorsFor('url').forEach(function(error){
+        newModel.get('errors').add(error.attribute, error.message);
+      });
+    }
+    return newModel
+	},
+  resetController: function(controller, isExiting, transition) {
+    // Reset the controller loading state before transition
+    if (isExiting) {
+      controller.set('isLoading', false);
+    }
+  }
 });
