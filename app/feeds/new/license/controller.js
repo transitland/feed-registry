9474484c1ebb641f522b589f6e5f2089c97e5b98
Meghan <mmhade@gmail.com>
2016-02-03 19:32:26 -0800

    reset license form and license attributes when the user clicks no for either yes/no toggle

--- app/feeds/new/license/controller.js
+++ app/feeds/new/license/controller.js
@@ -10,5 +10,24 @@ export default Ember.Controller.extend({
       this.set('model.license_attribution_text', null);
       return false;
     }
-  })
+  }),
+  resetInterpretations: function(){
+      this.set('model.license_use_without_attribution', null);
+      this.set('model.license_create_derived_product', null);
+      this.set('model.license_redistribute', null);
+  },
+  resetLicense: function(){
+    this.resetInterpretations();
+    this.interpret_license_mode = false;
+  },
+
+  actions: {
+    click: function(){
+      if (this.license_present_mode === false){
+        this.resetLicense();
+      } else if (this.interpret_license_mode === false){
+        this.resetInterpretations();
+      }
+    }
+  }
 });
