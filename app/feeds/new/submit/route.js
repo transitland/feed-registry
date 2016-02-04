dcf68d91e980913b445d1fc3d64d8dc822d73003
Meghan <mmhade@gmail.com>
2016-02-03 18:34:05 -0800

    remove comments

--- app/feeds/new/submit/route.js
+++ app/feeds/new/submit/route.js
@@ -2,12 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   createFeedFromGtfsService: Ember.inject.service('create-feed-from-gtfs'),
-  // model: function() {
-  //   return {
-  //     feed: this.get('createFeedFromGtfsService').feedModel,
-  //     user: this.get('createFeedFromGtfsService').userModel
-  //   };
-  // }
 	model: function() {
   		var feed = this.get('createFeedFromGtfsService').feedModel;
     	var user = this.get('createFeedFromGtfsService').userModel;
