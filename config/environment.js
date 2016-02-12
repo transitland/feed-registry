d8975e3028bfa2ca56e242fc8207157ed1244259
Drew Dara-Abrams <drew@mapzen.com>
2016-02-12 09:49:31 -0800

    again updating configuration to reflect HTTPs on dev/staging
    (again) fixes https://github.com/mapzen/transitland-internal/issues/7

--- config/environment.js
+++ config/environment.js
@@ -57,7 +57,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'staging') {
-    ENV.datastoreHost = 'http://dev.transit.land';
+    ENV.datastoreHost = 'https://dev.transit.land';
     ENV.allowEditingMode = true;
     ENV.baseURL = '/feed-registry';
     ENV.googleAnalytics = {
