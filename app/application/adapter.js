44b5e27e63ab86eb88a88480a2b634dd36468d15
Meghan <mmhade@gmail.com>
2016-02-19 14:11:54 -0800

    remove 5 per page limit

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -7,17 +7,5 @@ export default DS.RESTAdapter.extend({
 	namespace: 'api/v1',
 	headers: {
 		'Authorization': 'Token token=' + ENV.datastoreAuthToken
-	},
-	ajaxOptions: function(url, type, options) {
-		var hash = this._super(url, type, options);
-		if (type === 'GET') {
-			let data = {};
-			if (typeof(hash.data) === 'string') {
-				data = JSON.parse(hash.data);
-			}
-			data["per_page"] = 5;
-			hash.data = data;
-		}
-		return hash;
 	}
 });
--- app/index/route.js
+++ app/index/route.js
@@ -3,7 +3,6 @@ import PaginatedRoute from 'feed-registry/mixins/paginated-route';
 
 export default Ember.Route.extend(PaginatedRoute, {
 	model: function(params) {
-
-            return this.store.query('operator', params);
-        }
+		return this.store.query('operator', params);
+	}
 });
