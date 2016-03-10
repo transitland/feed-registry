cab37c88b864514c6565af488ca371f0ace267af
Meghan <mmhade@gmail.com>
2016-03-10 13:05:34 -0800

    add jeojson helper, link to geojson in feed detail

---
+++ app/helpers/geojson-endpoint.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+import ENV from 'feed-registry/config/environment';
+
+
+export default Ember.Handlebars.registerBoundHelper('geojson-endpoint',function(param, text) {
+  var entity = '';
+  var baseURL = ENV.datastoreHost+'/api/v1/';
+
+  if (param.charAt(0) === 'o'){
+    entity = 'operators/';
+  } else if (param.charAt(0) === 'f'){
+    entity = 'feeds/';
+  }
+
+  if (param.charAt(1) === '-'){
+    return new Ember.Handlebars.SafeString('<a href =' + baseURL + entity + param +'.geojson target = "_blank">' + baseURL + entity + param + '</a>');
+  } else if (param.charAt(0) === '/') {
+    return new Ember.Handlebars.SafeString('<a href =' + ENV.datastoreHost + param + '.geojson target = "_blank">' + text + '</a>');
+  } else {
+    return new Ember.Handlebars.SafeString('<a href =' + baseURL + param + '.geojson target = "_blank">' + param + '</a>');
+  }
+});
+
