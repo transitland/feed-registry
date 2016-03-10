bfb1ee6ac7ff4e9d722c8ed1eb32649836d6a6f2
Meghan <mmhade@gmail.com>
2016-03-10 12:45:45 -0800

    templating for import level data, geoJSON, and schedule-stop-pair information

--- app/components/feed-detail/component.js
+++ app/components/feed-detail/component.js
@@ -1,5 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+
+	feedImportLevelZero: Ember.computed.gte('feed.import_level_of_active_feed_version', 0),
+	feedImportLevelOne: Ember.computed.gte('feed.import_level_of_active_feed_version', 1),
+	feedImportLevelTwo: Ember.computed.gte('feed.import_level_of_active_feed_version', 2),
+	feedImportLevelFour: Ember.computed.gte('feed.import_level_of_active_feed_version', 4),
+
+	feedImportLevelNull: function() {
+		return (this.get('import_level_of_active_feed_version') >= 0);
+  	}.property('feed.import_level_of_active_feed_version'),
+
+	scheduleStopPair: Ember.computed.gte('feed.import_level_of_active_feed_version', 2)
 	
 });
