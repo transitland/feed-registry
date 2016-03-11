4a75d17d41f46541a1f79625662e9ba833c5c7e3
Meghan <mmhade@gmail.com>
2016-03-10 16:48:32 -0800

    add check for import level 1 to feed detail controller

--- app/components/feed-detail/component.js
+++ app/components/feed-detail/component.js
@@ -2,4 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	feedImportLevelZero: Ember.computed.gte('feed.import_level_of_active_feed_version', 0),
+	feedImportLevelOne: Ember.computed.gte('feed.import_level_of_active_feed_version', 1),
+
 });
