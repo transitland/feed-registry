d394524eb23b8ace10541e7bcfaa6fe6c8c19790
Meghan <mmhade@gmail.com>
2016-03-15 11:54:06 -0700

    pass in text to be rendered into the json-endpoint helper

--- app/helpers/json-endpoint.js
+++ app/helpers/json-endpoint.js
@@ -2,9 +2,14 @@ import Ember from 'ember';
 import ENV from 'feed-registry/config/environment';
 
 
-export default Ember.Handlebars.registerBoundHelper('json-endpoint',function(param, attr) {
+export default Ember.Handlebars.registerBoundHelper('json-endpoint',function(param, attr, text) {
   var entity = '';
   var baseURL = ENV.datastoreHost+'/api/v1/';
+  // var text = "JSON";
+
+  if (text === undefined) {
+    text = "JSON";
+  }
 
   if (param.charAt(0) === 'o'){
     entity = 'operators/';
@@ -13,16 +18,16 @@ export default Ember.Handlebars.registerBoundHelper('json-endpoint',function(par
   }
 
   if (attr === "routes") {
-    return new Ember.Handlebars.SafeString('<a href =' + baseURL + 'routes?operatedBy=' + param +' target = "_blank">JSON</a>');
+    return new Ember.Handlebars.SafeString('<a href =' + baseURL + 'routes?operatedBy=' + param +' target = "_blank">' + text + '</a>');
   } else if (attr === "stops") {
-    return new Ember.Handlebars.SafeString('<a href =' + baseURL + 'stops?servedBy=' + param +' target = "_blank">JSON</a>');
+    return new Ember.Handlebars.SafeString('<a href =' + baseURL + 'stops?servedBy=' + param +' target = "_blank">' + text + '</a>');
   } else {
     if (param.charAt(1) === '-'){
-      return new Ember.Handlebars.SafeString('<a href =' + baseURL + entity + param +' target = "_blank">JSON</a>');
+      return new Ember.Handlebars.SafeString('<a href =' + baseURL + entity + param +' target = "_blank">' + text + '</a>');
     } else if (param.charAt(0) === '/') {
-      return new Ember.Handlebars.SafeString('<a href =' + ENV.datastoreHost + param + ' target = "_blank">JSON</a>');
+      return new Ember.Handlebars.SafeString('<a href =' + ENV.datastoreHost + param + ' target = "_blank">' + text + '</a>');
     } else {
-      return new Ember.Handlebars.SafeString('<a href =' + baseURL + param + ' target = "_blank">JSON</a>');
+      return new Ember.Handlebars.SafeString('<a href =' + baseURL + param + ' target = "_blank">' + text + '</a>');
     }
   }
 });
