8e844970cd21cd6bf48ebe50b27649e3fc0da0c0
Meghan <mmhade@gmail.com>
2016-03-18 17:20:32 -0700

    if response contains warnings, update feeds/new controller. Also update sub-controllers using inject and read

--- app/feeds/new/add-operator/controller.js
+++ app/feeds/new/add-operator/controller.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	feedsController: Ember.inject.controller('feeds.new'),
+	feedExists: Ember.computed.reads('feedsController.feedExists'),
 	operatorsIncluded: Ember.computed.mapBy('model.operators', 'include_in_changeset'),
 
 	singleOperator: Ember.computed('model.operators_in_feed', function(){
@@ -13,5 +15,3 @@ export default Ember.Controller.extend({
 	})
 	
 });
-
-
