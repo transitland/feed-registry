47be7adbe4b113210fa521d33c4c132954f67493
Meghan <mmhade@gmail.com>
2016-04-06 16:45:48 -0700

    handle multiple import levels

--- app/components/intro-copy/component.js
+++ app/components/intro-copy/component.js
@@ -1,8 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	importLevel: null,
+	mulitpleImportLevels: null,
+
 	importLevelFilterExists: function(){
-		return this.get('import_level');
+		if (this.importLevel === null){
+			this.importLevel = this.get('import_level');
+		}
+		return this.importLevel;
+	}.property('import_level'),
+
+	multipleImportLevelFiltersExist: function(){
+		if (this.get('import_level').indexOf(',') >= 1){
+			this.multipleImportLevels = this.get('import_level').split(',').map(Number);
+			this.import_level = Math.max.apply(Math,this.multipleImportLevels);
+			return true;
+		}
 	}.property('import_level'),
 
 	feedImportLevelZero: Ember.computed.gte('import_level', 0),
