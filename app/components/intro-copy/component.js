747e7fb67a841af0cc8c261bbca39ffd939da553
Meghan <mmhade@gmail.com>
2016-04-06 12:14:40 -0700

    dynamically render info about data available in filtered list

--- app/components/intro-copy/component.js
+++ app/components/intro-copy/component.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	importLevelFilterExists: function(){
+		console.log("IL: " + this.get('import_level'));
+		return this.get('import_level');
+	}.property('import_level'),
+
+	feedImportLevelZero: Ember.computed.gte('import_level', 0),
+	feedImportLevelOne: Ember.computed.gte('import_level', 1),
+	feedImportLevelTwo: Ember.computed.gte('import_level', 2),
+	feedImportLevelFour: Ember.computed.gte('import_level', 4),
+
 });
+
