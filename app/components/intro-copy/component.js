9beb7779bdc40e14db09771e1809484e811d2729
Meghan <mmhade@gmail.com>
2016-04-08 14:35:06 -0700

    handle filtering by unsupported import level

--- app/components/intro-copy/component.js
+++ app/components/intro-copy/component.js
@@ -4,25 +4,29 @@ export default Ember.Component.extend({
 	importLevel: null,
 	mulitpleImportLevels: null,
 
-	importLevelFilterExists: function(){
-		if (this.importLevel === null){
-			this.importLevel = this.get('import_level');
-		}
-		return this.importLevel;
-	}.property('import_level'),
-
-	multipleImportLevelFiltersExist: function(){
-		if (this.get('import_level').indexOf(',') >= 1){
-			this.multipleImportLevels = this.get('import_level').split(',').map(Number);
-			this.import_level = Math.max.apply(Math,this.multipleImportLevels);
-			return true;
-		}
-	}.property('import_level'),
-
 	feedImportLevelZero: Ember.computed.gte('import_level', 0),
 	feedImportLevelOne: Ember.computed.gte('import_level', 1),
 	feedImportLevelTwo: Ember.computed.gte('import_level', 2),
 	feedImportLevelFour: Ember.computed.gte('import_level', 4),
 
-});
+	importLevelFilterExists: function(){
+		if (this.get('import_level')) {
+			this.importLevel = this.get('import_level');
+			if (this.importLevel.includes(',')){
+				this.multipleImportLevels = this.get('import_level').split(',').map(Number);
+				this.importLevel = Math.max.apply(Math,this.multipleImportLevels);
+				this.import_level = this.importLevel;
+			} else {
+				this.import_level = this.importLevel;
+			}
+			return true;
+		}
+	}.property('import_level'),
 
+	supportedLevel: function(){
+		if (this.import_level >= 0 && this.import_level <= 4){
+			return true;
+		}
+	}.property('import_level')
+
+});
