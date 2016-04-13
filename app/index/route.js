3b9ef87d8e9b4c449c183b9817bfaafe1014993e
Meghan <mmhade@gmail.com>
2016-04-13 16:34:41 -0700

    clear query params when clicking to view All Feeds

--- app/index/route.js
+++ app/index/route.js
@@ -4,5 +4,10 @@ import PaginatedOrderedRoute from 'feed-registry/mixins/paginated-ordered-route'
 export default Ember.Route.extend(PaginatedOrderedRoute, {
 	model: function(params) {
 		return this.store.query('operator', params);
+	},
+	actions: {
+		queryParamsDidChange: function(){
+			this.refresh();
+		}
 	}
 });
