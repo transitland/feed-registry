7a40475af1599a119629637ead83aac551fdc9d8
Meghan <mmhade@gmail.com>
2016-05-09 14:51:31 -0700

    add new model attributes for name and short name

--- app/geography/model.js
+++ app/geography/model.js
@@ -3,5 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 	country: DS.attr(),
 	state: DS.attr(),
-	metro: DS.attr()  
+	metro: DS.attr(),
+	name: DS.attr(),
+	short_name: DS.attr()
 });
