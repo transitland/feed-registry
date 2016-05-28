cb254a71ffb9c774ab3bc144cce4069c46e606fa
Drew Dara-Abrams <drew@mapzen.com>
2016-05-27 18:14:45 -0700

    note for a future TODO

--- app/operators/show/controller.js
+++ app/operators/show/controller.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  // TODO: check import level across all feeds associated with an operator, not just the first
   operatorFirstFeed: Ember.computed.alias('model.feeds.firstObject'),
   linkToTurnByTurnDemo: Ember.computed('model.centroid', 'operatorFirstFeed.isAtLeastImportLevelFour', function () {
     if (this.get('operatorFirstFeed.isAtLeastImportLevelFour')) {
