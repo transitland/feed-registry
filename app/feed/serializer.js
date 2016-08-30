e39bd7d055d758aa6b260e27cf01378b8a86bb81
Ian Rees <ian@ianrees.net>
2016-08-30 00:59:01 -0700

    Serializer: primary key onestop_id

--- app/feed/serializer.js
+++ app/feed/serializer.js
@@ -1,10 +1,5 @@
 import ApplicationSerializer from '../application/serializer';
 
 export default ApplicationSerializer.extend({
-  primaryKey: 'onestop_id',
-	attrs: {
-		feeds: {
-			key: 'represented_in_feed_onestop_ids'
-		}
-	}
+  primaryKey: 'onestop_id'
 });
