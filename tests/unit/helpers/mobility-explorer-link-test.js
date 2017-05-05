680568889b4bc55930c67982fb1abfe72ca19d43
Drew Dara-Abrams <drew@mapzen.com>
2017-05-04 21:58:51 -0700

    add links from operator detail views to Mobility Explorer
    closes #367

---
+++ tests/unit/helpers/mobility-explorer-link-test.js
@@ -0,0 +1,10 @@
+import { mobilityExplorerLink } from '../../../helpers/mobility-explorer-link';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | mobility explorer link');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = mobilityExplorerLink(42);
+  assert.ok(result);
+});
