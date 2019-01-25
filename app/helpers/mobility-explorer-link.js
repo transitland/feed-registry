581c3c2a939918ce8646ed703465ecbc0bb43b94
Drew Dara-Abrams <dda@dara-abrams.com>
2019-01-24 20:54:29 -0800

    Mobility Explorer links

--- app/helpers/mobility-explorer-link.js
+++ app/helpers/mobility-explorer-link.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
 
 export function mobilityExplorerLink(params, hash) {
-  let link = 'https://mobility-explorer.netlify.com/#/'
-  if (hash.query == 'operator') {
+  let link = 'https://mobility-explorer.interline.io/#/';
+  if (hash.query === 'operator') {
     link = link + 'operators?bbox=' + hash.operator.get('geometryToBboxString') + '&onestop_id=' + hash.operator.get('onestop_id');
-  } else if (hash.query == 'routesByOperator') {
+  } else if (hash.query === 'routesByOperator') {
     link = link + 'routes?bbox=' + hash.operator.get('geometryToBboxString') + '&operated_by=' + hash.operator.get('onestop_id');
-  } else if (hash.query == 'stopsByOperator') {
+  } else if (hash.query === 'stopsByOperator') {
     link = link + 'stops?bbox=' + hash.operator.get('geometryToBboxString') + '&served_by=' + hash.operator.get('onestop_id');
   }
   return new Ember.Handlebars.SafeString(`<a href="${link}">${hash.text}</a>`);
