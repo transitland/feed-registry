59cb4d21a6e9fb328337226bdc432170093cac0d
Drew Dara-Abrams <drew@interline.io>
2020-03-04 16:17:31 -0800

    Transitland API keys

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -16,10 +16,9 @@ export default DS.RESTAdapter.extend({
   },
   ajaxOptions: function (url, type, options) {
     var hash = this._super(url, type, options);
-    // only need to include api_key when making GET requests
-    // because those are the most frequent type of request.
-    // if we include api_key in POSTs or PUTs, Datastore will barf
-    //
+    hash.beforeSend = function (xhr) {
+      xhr.setRequestHeader('apikey', ENV.transitlandApiKey);
+    }
     if (typeof (ENV.apiProxyKey) !== "undefined" && type === 'GET') {
       let data = {};
       if (typeof (hash.data) === 'string') {
